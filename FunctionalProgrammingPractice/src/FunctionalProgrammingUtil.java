3b3347782ea7a4d535696da61d64a52156398e2b
R A <ra@Rs-MacBook-Air.local>
2022-08-14 21:32:50 -0400

    FunctionalProgramming CoinChangeProblem

--- FunctionalProgrammingPractice/src/FunctionalProgrammingUtil.java
+++ FunctionalProgrammingPractice/src/FunctionalProgrammingUtil.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class FunctionalProgrammingUtil {
 
     public static int flowerBoxProblem(int[] input) {
@@ -14,5 +16,42 @@ public class FunctionalProgrammingUtil {
             b = max;
         }
         return b;
+    }
+
+    static HashMap<String, Integer> memoizationCache = new HashMap<>();
+
+    static int[] denomination = new int[]{1, 2, 3};
+
+    public static int makeChange(int denominationIndex, int target) {
+        if (memoizationCache.containsKey(denominationIndex + "," + target)) {
+            return memoizationCache.get(denominationIndex + "," + target);
+        }
+        int choiceTake = 0;
+        int choiceLeave;
+        int optimal;
+        int coinValue = denomination[denominationIndex];
+
+        //choiceTake: the total least number of coins we need if we choose to take a coin
+        if (target < coinValue) {
+            //coin to large
+            choiceTake = Integer.MAX_VALUE;
+        } else if (coinValue == target) {
+            //value reached
+            choiceTake = 1;
+        } else if (coinValue < target) {
+            //take 1 coin and recurse
+            choiceTake = 1 + makeChange(denominationIndex, target - coinValue);
+        }
+
+        //choiceLeave: the total least number of coins we need if we choose to leave the current a coin denomination
+        if (denominationIndex == 0 || coinValue == target) {
+            choiceLeave = Integer.MAX_VALUE;
+        } else {
+            choiceLeave = makeChange(denominationIndex - 1, target);
+        }
+
+        optimal = Math.min(choiceLeave, choiceTake);
+        memoizationCache.put(denominationIndex + "," + target, optimal);
+        return optimal;
     }
 }
--- FunctionalProgrammingPractice/src/Main.java
+++ FunctionalProgrammingPractice/src/Main.java
@@ -1,10 +1,13 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello world!");
         int[] flowerBoxArray = new int[]{3, 10, 3, 1, 2};
         int[] flowerBoxArray2 = new int[]{9, 10, 9};
         System.out.println(FunctionalProgrammingUtil.flowerBoxProblem(flowerBoxArray));
-        System.out.println(FunctionalProgrammingUtil.flowerBoxProblem(flowerBoxArray2));
+        System.out.println(FunctionalProgrammingUtil.makeChange(2,5));
+        System.out.println(FunctionalProgrammingUtil.makeChange(2,8));
+        System.out.println(FunctionalProgrammingUtil.makeChange(2,2));
     }
 }
